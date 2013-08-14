701e540dbabc616b1682e529ed62bf5946e002d3
Thomas Sader <thommey@gmail.com>
2013-08-14 17:58:12 +0200

    add safe getters and setters for config values

--- include/config.h
+++ include/config.h
@@ -2,6 +2,8 @@
 #define CONFIG_H_
 
 int load_config(const char *path);
+char *config_get(const char *section, const char *key);
+void config_set(const char *section, const char *key, const char *value);
 void print_config(void);
 
 #endif // CONFIG_H_
